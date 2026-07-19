ac419615b4b0644a5b51c07a144e130179a0c60c
Lolo-arch24 <ummusalama024@gmail.com>
2026-07-19 01:00:56 +0100

    Add tool UI, tree & enemy VFX; default tool AXE
    Introduce tool UI and textures and change player default tool to AXE. Add Tree object (scene + script) with apple spawning and hit behavior, and place a Tree instance in the level. Add enemy VFX (Enemy scene + script), a FlashSprite2D helper and a flash shader. Update level logic: comment debug physics, register tool hits for AXE and SWORD to interact with nearby objects. Update player scene to include the ToolUI PackedScene. Multiple new scenes/scripts/resources added to support these features.

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -4,13 +4,13 @@ var plant_scene = preload("res://scenes/objects/plant.tscn")
 var used_cells: Array[Vector2i]
 @onready var player = $Objects/Player
 
-func _physics_process(_delta: float) -> void:
-	var pos = player.position + player.last_direction * 16 + Vector2(0,4)
-	var grid_coord: Vector2i = Vector2i(int(pos.x / Data.TILE_SIZE),int(pos.y / Data.TILE_SIZE))
-	grid_coord.x += -1 if pos.x < 0 else 0
-	grid_coord.y += -1 if pos.x < 0 else 0
-	$Layers/DebugLayer.clear()
-	$Layers/DebugLayer.set_cell(grid_coord, 0, Vector2i(1,3))
+#func _physics_process(_delta: float) -> void:
+	#var pos = player.position + player.last_direction * 16 + Vector2(0,4)
+	#var grid_coord: Vector2i = Vector2i(int(pos.x / Data.TILE_SIZE),int(pos.y / Data.TILE_SIZE))
+	#grid_coord.x += -1 if pos.x < 0 else 0
+	#grid_coord.y += -1 if pos.x < 0 else 0
+	#$Layers/DebugLayer.clear()
+	#$Layers/DebugLayer.set_cell(grid_coord, 0, Vector2i(1,3))
 
 func _on_player_tool_use(tool: Enum.Tool, pos: Vector2) -> void:
 	var grid_coord: Vector2i = Vector2i(int(pos.x / Data.TILE_SIZE), int(pos.y / Data.TILE_SIZE))
@@ -35,3 +35,7 @@ func _on_player_tool_use(tool: Enum.Tool, pos: Vector2) -> void:
 				var plant = plant_scene.instantiate()
 				plant.setup(grid_coord, $Objects)
 				used_cells.append(grid_coord)
+		Enum.Tool.AXE, Enum.Tool.SWORD:
+			for object in get_tree().get_nodes_in_group('Objects'):
+				if object.position.distance_to(pos) < 20:
+					object.hit(tool)
